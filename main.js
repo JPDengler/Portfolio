ecfc22328ab2dd14e90bc4c9771db4adea00f354
Joey <JPDengler@users.noreply.github.com>
2024-05-13 19:56:44 -0400

    modified:   main.js

--- main.js
+++ main.js
@@ -19,10 +19,9 @@ Available commands:
                 `);
                 break;
             case 'showprojx':
-                output(`
-Project 1: Description of project 1...
-Project 2: Description of project 2...
-Project 3: Description of project 3...
+                output(`CS-330 Final: 3D Rendering of my desk
+CS-320 Final: Software Test Automation
+CS-300: Data Structures and Algorithms
                 `);
                 break;
             default:
