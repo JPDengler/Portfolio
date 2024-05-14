32b3d02bd465376bc770ba41bc608a0251107a82
Joey <JPDengler@users.noreply.github.com>
2024-05-13 20:05:53 -0400

    modified:   main.js

--- main.js
+++ main.js
@@ -18,12 +18,22 @@ Available commands:
 - showprojx: Preview some completed favorite projects.
                 `);
                 break;
+
             case 'showprojx':
-                output(`CS-330 Final: 3D Rendering of my desk
+                output(`Some recent favorites:
+
+CS-330 Final: 3D Rendering of my desk
 CS-320 Final: Software Test Automation
 CS-300: Data Structures and Algorithms
                 `);
                 break;
+
+            case 'currentwork':
+                output(`ProjectBMO - Creating a real life BMO
+                from the show Adventure Time with RaspyberryPI,
+                Python, OpenAI, and speech recongition!
+                `);
+                
             default:
                 output(`Command not recognized: ${cmd}\nPlease type HELP for assistance.`);
                 break;
