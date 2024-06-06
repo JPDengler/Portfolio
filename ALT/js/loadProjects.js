70330fada21abcf605eba2cf49e584ff22fac890
Joey <JPDengler@users.noreply.github.com>
2024-06-06 19:18:00 -0400

    ghfdsjh

--- ALT/js/loadProjects.js
+++ ALT/js/loadProjects.js
@@ -13,6 +13,7 @@ async function loadProjects() {
                 <h3>${project.title}</h3>
                 <p>${project.description}</p>
                 <a href="${project.link}" target="_blank">View Project</a>
+                <p>'<br>'<p>
             `;
 
             if (project.showcase) {
