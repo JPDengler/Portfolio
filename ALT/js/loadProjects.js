5ded018e45d6e7721dfcc7dcb8f0ecd22ef696c0
Joey <JPDengler@users.noreply.github.com>
2024-06-09 19:25:32 -0400

    Update loadProjects.js

--- ALT/js/loadProjects.js
+++ ALT/js/loadProjects.js
@@ -13,7 +13,6 @@ async function loadProjects() {
                 <h3>${project.title}</h3>
                 <p>${project.description}</p>
                 <a href="${project.link}" target="_blank">View Project</a>
-                <p></p>
             `;
 
             if (project.showcase) {
